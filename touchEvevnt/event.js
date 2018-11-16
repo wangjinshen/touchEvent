331562a1b3cec601e6e6cdd4d955b28d75bc2a8c
wangjinshen <807253330@qq.com>
2018-11-16 10:26:11 +0800

    event

--- touchEvevnt/event.js
+++ touchEvevnt/event.js
@@ -1,112 +1,222 @@
-(function (window){  //传入window，提高变量的查找效率
-    function myQuery(selector){  //这个函数就是对外提供的接口。
-        //调用这个函数的原型对象上的_init方法，并返回
-        return myQuery.prototype._init(selector);
-    }
-    myQuery.prototype = {
-        /*初始化方法，获取当前query对象的方法*/
-        _init: function (selector){
-            if (typeof selector == "string"){
-                //把查找到的元素存入到这个原型对象上。
-                this.ele = window.document.querySelector(selector);
-                //返回值其实就是原型对象。
-                return this;
-            }
-        },
-        /*单击事件：
-         * 为了规避click的300ms的延迟，自定义一个单击事件
-         * 触发时间：
-         *   当抬起手指的时候触发
-         *   需要判断手指落下和手指抬起的事件间隔，如果小于500ms表示单击时间。
-         *
-         *   如果是大于等于500ms，算是长按时间
-         * */
-        tap: function (handler){
-            this.ele.addEventListener("touchstart", touchFn);
-            this.ele.addEventListener("touchend", touchFn);
+function $(dom) {
+    return $.prototype.init(dom)
+}
 
-            var startTime,
-                endTime;
+$.prototype = {
 
-            function touchFn(e){
-                e.preventDefault()
-                switch (e.type){
-                    case "touchstart":
-                        startTime = new Date().getTime();
-                        break;
-                    case "touchend":
-                        endTime = new Date().getTime();
-                        if (endTime - startTime < 500){
-                            handler.call(this, e);
-                        }
-                        break;
-                }
-            }
-        },
-        /**
-         * 长按
-         * @param handler
-         */
-        longTag: function (handler){
-            this.ele.addEventListener("touchstart", touchFn);
-            this.ele.addEventListener("touchmove", touchFn);
-            this.ele.addEventListener("touchend", touchFn);
-            var timerId;
+    init(el) {
+        if (typeof el == "string") {
+            let arr = el.split(" ")
+            this.el = this.elEvery(arr)
 
-            function touchFn(e){
-                switch (e.type){
-                    case "touchstart" :  //500ms之后执行
-                        timerId = setTimeout(function (){
-                            handler.call(this, e);
-                        }, 500)
-                        break;
-                    case "touchmove" :
-                        //如果中间有移动也清除定时器
-                        clearTimeout(timerId)
-                        break;
-                    case "touchend" :
-                        //如果在500ms之内抬起了手指，则需要定时器
-                        clearTimeout(timerId);
-                        break;
-                }
-            }
-        },
-        /**
-         * 左侧滑动。
-            记录手指按下的左边，在离开的时候计算 deltaX是否满足左滑的条件
-         *
-         */
-        slideLeft: function (handler){
-            this.ele.addEventListener("touchstart", touchFn);
-            this.ele.addEventListener("touchend", touchFn);
-            var startX, startY, endX, endY;
-
-            function touchFn(e){
-                e.preventDefault();
-                var firstTouch = e.changedTouches[0];
-                switch (e.type){
-                    case "touchstart":
-                        startX = firstTouch.pageX;
-                        startY = firstTouch.pageY;
-                        break;
-                    case "touchend":
-                        endX = firstTouch.pageX;
-                        endY = firstTouch.pageY;
-//x方向移动大于y方向的移动，并且x方向的移动大于25个像素，表示在向左侧滑动
-                        if (Math.abs(endX - startX) >= Math.abs(endY - startY) && startX - endX >= 25){
-                            handler.call(this, e);
-                        }
-                        break;
-                }
-            }
-        },
-        /**
-         * 右侧滑动。
-         *
-         */
-        rightLeft: function (e){
-            //TODO:
+        } else {
+            this.el = el
         }
-    }
-    window.$ = window.myQuery = myQuery;
-})(window);
+
+        return this
+    },
+
+    elEvery(arr) {
+
+        let domArr = []
+
+        function every(dom, el, arrIndex) {
+            dom.forEach((i) => {
+                if (arrIndex < arr.length - 1) {
+                    every(Array.from(i.querySelectorAll(el)), arr[arrIndex + 1], arrIndex + 1)
+                } else {
+                    // domArr=[...domArr,...Array.from(i.querySelectorAll(el))]
+                    domArr.push(...Array.from(i.querySelectorAll(el)))
+                }
+            })
+        }
+        every([document], arr[0], 0)
+        return domArr
+    },
+    attr(attr, val) {
+        if (!val) {
+            return this.el.getAttribute(attr)
+        } else {
+            this.el.setAttribute(attr, val)
+            return this
+        }
+    },
+    tap(cb) {
+        let start, end, state = true
+        if (this.el instanceof Array) {
+            this.el.forEach((i) => {
+                i.addEventListener("touchstart", handle)
+                i.addEventListener("touchmove", handle)
+                i.addEventListener("touchend", handle)
+            });
+        } else {
+            this.el.addEventListener("touchstart", handle)
+            this.el.addEventListener("touchmove", handle)
+            this.el.addEventListener("touchend", handle)
+        }
+        return this
+
+
+        function handle(e) {
+            switch (e.type) {
+                case "touchstart":
+                    {
+                        start = new Date().getTime()
+                        state = true
+                    }
+                    break;
+                case "touchmove":
+                    {
+                        state = false
+                    }
+                    break;
+                case "touchend":
+                    {
+                        end = new Date().getTime()
+                        if (end - start <= 300 && state) {
+                            cb(e)
+                        }
+                    }
+                    break;
+            }
+        }
+
+    },
+    longtap(cb) {
+        let time
+        if (this.el instanceof Array) {
+            this.el.forEach((i) => {
+                i.addEventListener("touchstart", handle)
+                i.addEventListener("touchmove", handle)
+                i.addEventListener("touchend", handle)
+            });
+        } else {
+            this.el.addEventListener("touchstart", handle)
+            this.el.addEventListener("touchmove", handle)
+            this.el.addEventListener("touchend", handle)
+        }
+        return this
+
+        function handle(e) {
+
+            switch (e.type) {
+                case "touchstart":
+                    {
+                        time = setTimeout(cb, 300)
+                    }
+                    break;
+                case "touchmove":
+                    {
+                        clearTimeout(time)
+                    }
+                    break;
+                case "touchend":
+                    {
+                        clearTimeout(time)
+                    }
+                    break;
+            }
+        }
+    },
+    move(type, cb) {
+        let startX, endX, startY, endY
+        if (this.el instanceof Array) {
+            this.el.forEach((i) => {
+                i.addEventListener("touchstart", handle)
+
+                i.addEventListener("touchend", handle)
+            });
+        } else {
+            this.el.addEventListener("touchstart", handle)
+
+            this.el.addEventListener("touchend", handle)
+        }
+
+
+        function handle(e) {
+            switch (e.type) {
+                case "touchstart":
+                    {
+                        startX = e.changedTouches[0].pageX
+                        startY = e.changedTouches[0].pageY
+                    }
+                    break;
+                case "touchend":
+                    {
+                        endX = e.changedTouches[0].pageX
+                        endY = e.changedTouches[0].pageY
+                        switch (type) {
+                            case "moveLeft":
+                                {
+                                    if (startX - endX > 25) {
+                                        if (Math.abs(startY - endY) < Math.abs(startX - endX)) {
+
+                                            cb(e)
+                                        }
+                                    }
+                                }
+                                break;
+                            case "moveRight":
+                                {
+                                    if (endX - startY > 25) {
+                                        if (Math.abs(startY - endY) < Math.abs(startX - endX)) {
+
+                                            cb(e)
+                                        }
+                                    }
+                                }
+                                break;
+                            case "moveTop":
+                                {
+                                    if (startY - endY > 25) {
+                                        if (Math.abs(startY - endY) > Math.abs(startX - endX)) {
+                                            cb(e)
+                                        }
+                                    }
+                                }
+                                break;
+                            case "moveBottom":
+                                {
+                                    if (endY - startY > 25) {
+                                        if (Math.abs(startY - endY) > Math.abs(startX - endX)) {
+                                            cb(e)
+                                        }
+                                    }
+                                }
+                                break;
+                        }
+
+                    }
+                    break;
+            }
+        }
+
+
+
+
+    },
+    moveLeft(cb) {
+
+        this.move("moveLeft", cb)
+
+        return this
+    },
+    moveRight(cb) {
+
+        this.move("moveRight", cb)
+
+        return this
+
+    },
+    moveTop(cb) {
+
+        this.move("moveTop", cb)
+        return this
+
+    },
+    moveBottom(cb) {
+
+        this.move("moveBottom", cb)
+        return this
+    },
+}
